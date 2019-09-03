c555dbd7910625a6a36f397d25244427fa372c00
VRtistHan <54020605+VRtistHan@users.noreply.github.com>
2019-09-03 13:41:36 +0900

    MovingHoop(continue)

--- Assets/MovingHoop.cs
+++ Assets/MovingHoop.cs
@@ -5,18 +5,17 @@ using UnityEngine;
 public class MovingHoop : MonoBehaviour
 {
     float delta = -0.005f;
+
+    Transform originalPos;
     // Start is called before the first frame update
     void Start()
     {
+        Transform originalPos = GetComponent<Transform>();
 
     }
         
-
-
-    // Update is called once per frame
-    void Update()
-    {        
-        float newXPos = transform.position.x + delta;
+      public void HoopMove(){
+          float newXPos = transform.position.x + delta;
         transform.position = new Vector3(newXPos, transform.position.y, transform.position.z);
         if(transform.position.x <= 0){
             delta *= -1.0f;
@@ -24,5 +23,16 @@ public class MovingHoop : MonoBehaviour
         else if(transform.position.x >= 0.8){
             delta *= -1.0f;
         }
+      }  
+
+      public void StopMove(){
+          transform.position = originalPos.position;
+      }
+
+
+    // Update is called once per frame
+    void Update()
+    {        
+        
     }
 }
